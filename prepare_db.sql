d8e84fce8baff99ea200c1fa8d41c5f58ae5a71f
ChehAchraf <achrafchehboun@outlook.com>
2024-12-07 11:53:17 +0100

    add the file that create the db

---
+++ prepare_db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS revforme;
+USE revforme;
+
+CREATE TABLE IF NOT EXISTS cliente (
+    id_client INT(11) NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    telephone VARCHAR(15) NOT NULL,
+    adresse TEXT NOT NULL,
+    date_naissance DATE NOT NULL,
+    PRIMARY KEY (id_client)
+);
+
+CREATE TABLE IF NOT EXISTS activite (
+    id_activite INT(11) NOT NULL AUTO_INCREMENT,
+    titre VARCHAR(150) NOT NULL,
+    description VARCHAR(100),
+    destination VARCHAR(200) NOT NULL,
+    prix DECIMAL(10,2) NOT NULL,
+    date_debut DATE NOT NULL,
+    date_fin DATE NOT NULL,
+    places_disponible INT(11),
+    PRIMARY KEY (id_activite)
+);
+
+CREATE TABLE IF NOT EXISTS reservation (
+    id_reservation INT(11) NOT NULL AUTO_INCREMENT,
+    id_client INT(11) NOT NULL,
+    id_activite INT(11) NOT NULL,
+    date_reservation TIMESTAMP NOT NULL,
+    status ENUM('En attente', 'Confirmée', 'Annulée') NOT NULL,
+    PRIMARY KEY (id_reservation),
+    FOREIGN KEY (id_client) REFERENCES cliente(id_client),
+    FOREIGN KEY (id_activite) REFERENCES activite(id_activite)
+);
